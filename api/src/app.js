10754073a11c8bc937174f8465309dee82882b47
Victor Ramirez <vicdeveloper83@gmail.com>
2022-12-15 23:00:07 +0000

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -4,10 +4,10 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
 let cors = require('cors');
-server.use(cors());
 require('./db.js');
 
 const server = express();
+server.use(cors());
 
 server.name = 'API';
 
