f216175c518e2793ffcbf8c1af452bc1d9c0fc39
Victor Ramirez <vicdeveloper83@gmail.com>
2022-12-15 22:57:09 +0000

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -16,16 +16,13 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-	res.header(
-		'Access-Control-Allow-Origin',
-		'https://dogs-deploy-two.vercel.app/'
-	); // update to match the domain you will make the request from
-	res.header('Access-Control-Allow-Credentials', 'true');
-	res.header(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept'
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'Content-Type',
+		'Authorization'
 	);
-	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
 	next();
 });
 
